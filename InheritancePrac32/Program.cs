9199c8b12aebf3295d7221c3725b669c860c0bd8
Gihun Ham <ham@e-mirim.hs.kr>
2023-06-01 09:02:43 +0900

    강아지, 고양이 개별 클래스 구현

--- InheritancePrac32/Program.cs
+++ InheritancePrac32/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace InheritancePrac32
     {
         static void Main(string[] args)
         {
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+
+            foreach (var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+
+
         }
     }
 }
